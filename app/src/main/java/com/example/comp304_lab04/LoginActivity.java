a0e31bf98691cbdd44c048615dfab1e1389794d4
DogameLee <80062879+DogyeomLee@users.noreply.github.com>
2022-11-16 21:46:01 -0500

    can send nurse ID

--- app/src/main/java/com/example/comp304_lab04/LoginActivity.java
+++ app/src/main/java/com/example/comp304_lab04/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,7 +26,9 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        EditText username = findViewById(R.id.user_name);
+        EditText userfname = findViewById(R.id.user_fname);
+        EditText userlname = findViewById(R.id.user_lname);
+        EditText userdepartment = findViewById(R.id.userdepartment);
         EditText userid = findViewById(R.id.user_id);
         EditText password = findViewById(R.id.user_password);
 
@@ -33,9 +36,17 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                if(username.getText().toString().isEmpty())
+                if(userfname.getText().toString().isEmpty())
                 {
-                    Toast.makeText(getBaseContext(), "Write User Name", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getBaseContext(), "Write First Name", Toast.LENGTH_SHORT).show();
+                }
+                if(userlname.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getBaseContext(), "Write Last Name", Toast.LENGTH_SHORT).show();
+                }
+                if(userdepartment.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getBaseContext(), "Write User department", Toast.LENGTH_SHORT).show();
                 }
                 if(userid.getText().toString().isEmpty())
                 {
@@ -47,11 +58,13 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 else
                 {
+
                     Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                    intent.putExtra("nurseID", userid.getText().toString());
                     startActivity(intent);
                 }
-
             }
         });
+
     }
 }
--- app/src/main/java/com/example/comp304_lab04/MainActivity2.java
+++ app/src/main/java/com/example/comp304_lab04/MainActivity2.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -13,6 +15,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        TextView nurseId = (TextView) findViewById(R.id.textView2);
+        Intent intent2 = getIntent();
+        String nurseID = intent2.getStringExtra("nurseID");
+        nurseId.setText(nurseID);
 
         Button btn1 = (Button) findViewById(R.id.patientbut);
         btn1.setOnClickListener(new View.OnClickListener() {
